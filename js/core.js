6be6d0348cee654371be20f015d2c94ec9a45466
MalikaMarselovna <mmukashova1@gmail.com>
2024-01-26 18:03:15 +0600

    fix some blocks

--- js/core.js
+++ js/core.js
@@ -1,48 +1,12 @@
-var $cards = $('.card-object'),
-    $faceButtons = $('.face');
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
 
-$faceButtons.on('click', flipCard);
-
-function flipCard(event) {
-    event.preventDefault();
-
-    var $btnFace = $(this),
-        $card = $btnFace.parent('.card-object');
-
-    if ($card.hasClass('flip-in')) {
-        closeCards();
-    } else {
-        closeCards();
-        openCard($card);
-    }
-
-}
-
-function closeCards() {
-    $cards.each(function () {
-        $(this)
-            .filter('.flip-in')
-            .removeClass('flip-in')
-            .queue(function () {
-                // Force reflow hack
-                var reflow = this.offsetHeight;
-                $(this)
-                    .addClass('flip-out')
-                    .dequeue();
-            })
-
-    });
-}
-
-function openCard($card) {
-    $card
-        .removeClass('flip-out')
-        .queue(function () {
-            // Force reflow hack
-            var reflow = this.offsetHeight;
-            $(this)
-                .addClass('flip-in')
-                .dequeue();
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
         });
-
-}
+    });
+};
